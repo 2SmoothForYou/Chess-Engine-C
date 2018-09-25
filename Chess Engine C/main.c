d55941ce90819ea77a496cfd4b0bea6fc84e7f88
2SmoothForYou <jricart2001@gmail.com>
2018-09-24 21:29:06 -0400

    Initial Commit

---
+++ Chess Engine C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Chess Engine C
+//
+//  Created by Jaime Ricart on 9/24/18.
+//  Copyright © 2018 Jaime Ricart. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
