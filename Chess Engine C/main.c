cd82526c4c8ed09bea9968f4b7c6af344c1285d6
2SmoothForYou <jricart2001@gmail.com>
2018-09-24 22:55:34 -0400

    Added piece lists and Assert Testing Functionality

--- Chess Engine C/main.c
+++ Chess Engine C/main.c
@@ -13,19 +13,6 @@ int main(int argc, const char * argv[]) {
     
     AllInit();
     
-    int index = 0;
-    
-    for(index = 0; index < BRD_SQR_NUM; ++index) {
-        if(index % 10 == 0) printf("\n");
-        printf("%5d", Sq120ToSq64[index]);
-    }
-    
-    printf("\n\n");
-    
-    for(index = 0; index < 64; ++index) {
-        if(index % 8 == 0) printf("\n");
-        printf("%5d", Sq64ToSq120[index]);
-    }
-    
     return 0;
+    
 }
